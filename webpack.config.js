857467a4356e9e05758b656b7ecdb9837496a604
Sawyer Pollard <19192015+sawyerpollard@users.noreply.github.com>
2022-01-12 13:22:41 -0500

    Handle Chrome and Firefox builds through Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,76 +9,79 @@ const TerserPlugin = require('terser-webpack-plugin');
 const glob = require('glob');
 
 
-module.exports = {
-    mode: 'production',
-    entry: {
-        newtab: './src/newtab/newtab.ts',
-        popup: './src/popup/popup.ts',
-        background: './src/background.ts',
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/newtab/newtab.html',
-            filename: './newtab.html',
-            chunks: ['newtab'],
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/popup/popup.html',
-            filename: './popup.html',
-            chunks: ['popup'],
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/icons', to: 'icons' },
-                { from: 'src/images', to: 'images' },
-                { from: 'src/_locales', to: '_locales' },
-                { from: 'src/manifest.json', to: 'manifest.json' },
-            ],
-        }),
-        new PurgecssPlugin({
-            paths: glob.sync('./src/**/*', { nodir: true }),
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'postcss-loader',
-                    },
+module.exports = (env) => {
+    const target = env.target ? env.target : 'chrome';
+    return {
+        mode: 'production',
+        entry: {
+            newtab: './src/newtab/newtab.ts',
+            popup: './src/popup/popup.ts',
+            background: './src/background.ts',
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: './src/newtab/newtab.html',
+                filename: './newtab.html',
+                chunks: ['newtab'],
+            }),
+            new HtmlWebpackPlugin({
+                template: './src/popup/popup.html',
+                filename: './popup.html',
+                chunks: ['popup'],
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css',
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: 'src/icons', to: 'icons' },
+                    { from: 'src/images', to: 'images' },
+                    { from: 'src/_locales', to: '_locales' },
+                    { from: `src/manifest.${target}.json`, to: 'manifest.json' },
                 ],
-            },
+            }),
+            new PurgecssPlugin({
+                paths: glob.sync('./src/**/*', { nodir: true }),
+            }),
         ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    optimization: {
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ],
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        },
+                        {
+                            loader: 'css-loader',
+                        },
+                        {
+                            loader: 'postcss-loader',
+                        },
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        optimization: {
+            minimizer: [
+                new CssMinimizerPlugin(),
+                new TerserPlugin(),
+            ],
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+    };
 };
